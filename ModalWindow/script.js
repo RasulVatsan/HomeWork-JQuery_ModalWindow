e466ecb7ea0cbce96a3c016bc210abe62cab3a0d
RasulVatsan <rasul.vatsan@gmail.com>
2024-08-02 08:25:52 +0300

    Add files via upload

---
+++ ModalWindow/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#openWindow").click(function () {
+        $("#myWindow").fadeIn();
+    });
+
+    $("#closeWindow").click(function () {
+        $("#myWindow").fadeOut();
+    });
+});
